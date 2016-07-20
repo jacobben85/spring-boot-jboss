102d838f73e38ef6d76eb50889dca678530d1c33
Jacob Benny John <jacobben85@gmail.com>
2016-07-20 11:53:04 -0400

    Spring boot basic on jBoss

---
+++ src/main/java/com/jbjohn/controller/RestApiController.java
@@ -0,0 +1,23 @@
+package com.jbjohn.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ */
+@RestController
+@RequestMapping(value = "/", produces = {"application/json"})
+public class RestApiController {
+
+    @RequestMapping("")
+    public Map<String, Object> home() {
+
+        Map<String, Object> model = new HashMap<>();
+        model.put("title", "Spring gradle");
+
+        return model;
+    }
+}
